92b682d762bac55f0b4a8afe13ddcd15959f3b30
Pasha <pavel.chrksv@gmail.com>
2024-04-02 18:04:25 +0200

    PROXY-7 add converter

--- nasa-client/asteroid-client.js
+++ nasa-client/asteroid-client.js
@@ -16,6 +16,19 @@ asteroidApi.interceptors.request.use((config) => {
     return Promise.reject(error);
 });
 
+function convertToResponse(asteroids) {
+    return Object.values(asteroids.near_earth_objects).flatMap(date => {
+        return date.map(asteroid => ({
+            id: asteroid.id,
+            name: asteroid.name,
+            diameter: asteroid.estimated_diameter.meters.estimated_diameter_min,
+            is_potentially_hazardous_asteroid: asteroid.is_potentially_hazardous_asteroid,
+            close_approach_date_full: asteroid.close_approach_data[0].close_approach_date_full,
+            relative_velocity: asteroid.close_approach_data[0].relative_velocity.kilometers_per_second
+        }));
+    });
+}
+
 function getAsteroids(startDate, endDate) {
     const url = '/feed';
     const params = {
@@ -25,12 +38,12 @@ function getAsteroids(startDate, endDate) {
 
     return asteroidApi.get(url, {params})
         .then(response => {
-            return response.data;
+            return convertToResponse(response.data);
         })
         .catch(error => {
             console.error('Error fetching asteroids:', error);
             throw error;
         });
-};
+}
 
 module.exports = {getAsteroids};
