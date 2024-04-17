125da78bd90960ba0fb35cd91a348848fe1603a1
Pavel <pavel.chrksv@gmail.com>
2024-04-18 00:10:26 +0200

    PROXY-9 add params

--- controllers/meteorsController.js
+++ controllers/meteorsController.js
@@ -1,7 +1,16 @@
 const { fetchMeteors } = require("../clients/nasa/asteroid");
+const moment = require("moment");
 
 function getMeteors(req, res) {
-  fetchMeteors('2024-03-25', '2024-03-25')
+  var { from, to } = req.query;
+
+  if (!from || !to) {
+    const now = moment();
+    from = now.format("YYYY-MM-DD");
+    to = now.format("YYYY-MM-DD");
+  }
+
+  fetchMeteors(from, to)
     .then((result) => {
       res.status(200).json(result);
     })
@@ -10,4 +19,42 @@ function getMeteors(req, res) {
     });
 }
 
-module.exports = getMeteors;
+function wereDangerousMeteors(req, res) {
+  var { from, to } = req.query;
+
+  if (!from || !to) {
+    const now = moment();
+    from = now.format("YYYY-MM-DD");
+    to = now.format("YYYY-MM-DD");
+  }
+
+  fetchMeteors(from, to)
+    .then((result) => {
+      const thereIsHazard = Array.from(result).some(item => item.is_potentially_hazardous_asteroid === true)
+      res.status(200).json(thereIsHazard);
+    })
+    .catch(error => {
+      res.status(500).json({ error: error });
+    });
+}
+
+function countMeteors(req, res) {
+  var { from, to } = req.query;
+
+  if (!from || !to) {
+    const now = moment();
+    from = now.format("YYYY-MM-DD");
+    to = now.format("YYYY-MM-DD");
+  }
+
+  fetchMeteors(from, to)
+    .then((result) => {
+      const meteorsAmount = Array.from(result).length;
+      res.status(200).json(meteorsAmount);
+    })
+    .catch(error => {
+      res.status(500).json({ error: error });
+    });
+}
+
+module.exports = { getMeteors, wereDangerousMeteors, countMeteors };
